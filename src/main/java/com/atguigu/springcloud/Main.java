0b4ef18dbd3fdd25a008544af5466399ea6b0941
duoji <duoji@atguigu.com>
2024-01-11 16:46:03 +0800

    second commit

--- src/main/java/com/atguigu/springcloud/Main.java
+++ src/main/java/com/atguigu/springcloud/Main.java
@@ -2,6 +2,8 @@ package com.atguigu.springcloud;
 
 public class Main {
     public static void main(String[] args) {
+
+        System.out.println("world!");
         System.out.println("world!");
     }
 }
