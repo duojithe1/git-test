dc875be5eb22dc1f2fca5aff25f6591678a5cdfa
duoji <duoji@atguigu.com>
2024-01-11 17:08:08 +0800

    push commit

--- src/main/java/com/atguigu/springcloud/Main.java
+++ src/main/java/com/atguigu/springcloud/Main.java
@@ -8,5 +8,6 @@ public class Main {
         System.out.println("world!");
         System.out.println("master test!");
         System.out.println("hot-fix test");
+        System.out.println("push test");
     }
 }
